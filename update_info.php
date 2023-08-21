6dbb7bdc9a9351da89fe9c0f7e7a67a08fe444fa
hebertclever <hebertclever@gmail.com>
2023-08-21 15:01:09 -0300

    diretório uploads

--- update_info.php
+++ update_info.php
@@ -8,53 +8,47 @@ $dbname = "formulario_hebert";
 
 $conn = new mysqli($servername, $db_username, $password, $dbname);
 
-$username = $_SESSION['username'];
+$email = $_SESSION['email'];
 $name = $_POST['name'];
 $bio = $_POST['bio'];
 $phone = $_POST['phone'];
-$email = $_POST['email'];
 
-if ($_FILES['url_img']['size'] > 70000) {
-    echo "A imagem é muito grande (limite de 70KB).";
-    exit();
+
+$url_img = "";
+
+// Verifica se um arquivo foi carregado
+if ($_FILES['url_img']['error'] == UPLOAD_ERR_OK) {
+    if ($_FILES['url_img']['size'] > 70000) {
+        echo "A imagem é muito grande (limite de 70KB).";
+        exit();
+    }
+
+    $target_dir = "uploads/";
+    $target_file = $target_dir . basename($_FILES["url_img"]["name"]);
+    $imageFileType = pathinfo($target_file, PATHINFO_EXTENSION);
+
+
+    if (!in_array($imageFileType, ["jpg", "jpeg", "png"])) {
+        echo "Formato de arquivo não suportado.";
+        exit();
+    }
+
+    
+    if (!move_uploaded_file($_FILES["url_img"]["tmp_name"], $target_file)) {
+        echo "Ocorreu um erro ao carregar a imagem.";
+        exit();
+    }
+
+    $url_img = $target_file; 
 }
 
-$target_dir = "uploads/";
-$target_file = $target_dir . basename($_FILES["url_img"]["name"]);
-$imageFileType = pathinfo($target_file, PATHINFO_EXTENSION);
-
-if ($imageFileType == "jpg" || $imageFileType == "jpeg") {
-    $image = imagecreatefromjpeg($_FILES["url_img"]["tmp_name"]);
-} else if ($imageFileType == "png") {
-    $image = imagecreatefrompng($_FILES["url_img"]["tmp_name"]);
-} else {
-    echo "Formato de arquivo não suportado.";
-    exit();
-}
-
-$new_width = 200;
-$old_width = imagesx($image);
-$old_height = imagesy($image);
-$new_height = floor($old_height * ($new_width / $old_width));
-
-$new_image = imagecreatetruecolor($new_width, $new_height);
-imagecopyresampled($new_image, $image, 0, 0, 0, 0, $new_width, $new_height, $old_width, $old_height);
-
-$webp_file = $target_dir . basename($_FILES["url_img"]["name"], ".$imageFileType") . ".webp";
-imagewebp($new_image, $webp_file);
-
-$url_img = $webp_file; // Use this variable in the update query
-
-$sql = "UPDATE users SET name='$name', bio='$bio', phone='$phone', email='$email', url_img='$url_img' WHERE username='$username'";
+$sql = "UPDATE users SET name='$name', bio='$bio', phone='$phone', url_img='$url_img' WHERE email='$email'";
 
 if ($conn->query($sql) === TRUE) {
     header("Location: personal_info.php");
 } else {
     echo "Erro: " . $sql . "<br>" . $conn->error;
 }
-
-imagedestroy($image);
-imagedestroy($new_image);
 
 $conn->close();
 ?>
